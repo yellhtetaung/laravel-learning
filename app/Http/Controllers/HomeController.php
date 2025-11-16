c98c15fc270fcf62d8eddb45539fc3a6f48706bc
yehtetaung <yehtetaung.dev@gmail.com>
2025-11-16 21:15:42 +0630

    learn: finished 07

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $posts = Post::all();
+        return view('home', compact('posts'));
     }
 
     public function about()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
-    <main class="container mx-auto mt-5">
-        <h1 class="text-2xl text-red-500 font-bold">Home page</h1>
-    </main>
+    <div class="container col-12 col-md-10 col-lg-8">
+        @foreach ($posts as $post)
+            <div class="card mt-2">
+                <h5 class="card-header">{{ $post->name }}</h5>
+
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->name }}</h5>
+                    <p class="card-text">{{ $post->description }}</p>
+                    <a href="#" class="btn btn-primary">View</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </x-layout>
